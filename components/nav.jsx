00985f3654dab8c9819c1ce5f6435e4bd1aab0bd
nandanaruh <nandanaruh@gmail.com>
2025-06-26 00:52:04 +0530

    Add downward arrow

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { HiChevronDown } from "react-icons/hi";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import navLinks from "./navLinks";
@@ -13,7 +14,7 @@ export default function Nav() {
         <div key={index} className="relative group">
           <Link
             href={link.path}
-            className={`capitalize font-bold text-[16px] transition-normal
+            className={`capitalize font-bold text-[16px] flex items-center gap-1 transition-normal
               ${
                 link.path === pathname
                   ? "text-yellow-300"
@@ -22,15 +23,18 @@ export default function Nav() {
             `}
           >
             {link.name}
+            {link.children && (
+              <HiChevronDown className="text-medium text-white group-hover:text-yellow-300 transition-colors" />
+            )}
           </Link>
 
           {link.children && (
-            <div className="absolute left-0 mt-2 hidden group-hover:block shadow-lg rounded z-50 bg-gray-700 w-max min-w-full whitespace-nowrap">
+            <div className="absolute left-0 mt-1 hidden group-hover:block shadow-lg rounded z-50 bg-gray-600 w-max min-w-full whitespace-nowrap">
               {link.children.map((child, i) => (
                 <Link
                   key={i}
                   href={child.path}
-                  className="block px-4 py-2 text-medium font-semibold border-b border-gray-500 hover:bg-gray-600 text-white hover:text-yellow-400"
+                  className="block px-4 py-2 text-medium font-semibold border-b border-gray-400 hover:bg-gray-500 text-white hover:text-yellow-300"
                 >
                   {child.name}
                 </Link>
