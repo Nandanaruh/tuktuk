2835fabcdd0b26acbd51fc8f11709114ce963ef0
nandanaruh <nandanaruh@gmail.com>
2025-06-26 22:28:21 +0530

    Fix whatsapp button and text

--- components/lowSeason.jsx
+++ components/lowSeason.jsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function LowSeason() {
   return (
-    <div className="flex flex-row top-0 left-0 w-full h-[80px] items-center justify-items-center mx-auto pb-3 bg-amber-300">
+    <div className="flex flex-row top-0 left-0 w-full h-[80px] items-center justify-center mx-auto pb-3 bg-amber-300">
       <div>
         <p className="text-xl font-bold text-black">
           🚨 Low Season = Lower Prices! Score exclusive tuktuk deals now –
@@ -18,7 +18,7 @@ export default function LowSeason() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Button className="gap-2 rounded-full hover:bg-sky-800">
+          <Button className="w-[160px] gap-2 rounded-full hover:bg-sky-800 mx-2">
             CHAT WITH US!
           </Button>
         </Link>
