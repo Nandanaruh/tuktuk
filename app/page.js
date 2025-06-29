e142dccb8b991b8f6bbfd37c0fe2377c7560b14d
nandanaruh <nandanaruh@gmail.com>
2025-06-29 09:15:34 +0530

    Create component for unfogettable section with coconut tree

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Nav from "@/components/nav";
 import LowSeason from "@/components/lowSeason";
 import Background from "@/components/background";
 import BackgroundSvg from "@/components/backgroundSvg";
+import Unforgettable from "@/components/unforgettable";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       <section className="w-full h-[60px] bg-amber-300 z-50 text-white">
         <LowSeason />
       </section>
-      <section className="relative h-[2048px]">
+      <section className="relative h-full">
         <Background />
         <div className="absolute bg-gradient-to-b from-gray-950 top-0 left-0 w-full z-10">
           <div className="flex flex-row gap-4 items-center justify-evenly mx-40 py-2">
@@ -22,14 +23,7 @@ export default function Home() {
         </div>
         <BackgroundSvg />
       </section>
-      <section className="min-h-screen bg-white text-black px-20 py-32">
-        <h2 className="text-4xl font-bold mb-6">About Us</h2>
-        <p className="text-lg max-w-3xl leading-relaxed">
-          This is the next section of the page that loads after the full hero
-          image is revealed. Add your content like services, description, or
-          call to actions here.
-        </p>
-      </section>
+      <Unforgettable />
     </main>
   );
 }
---
+++ components/unforgettable.jsx
@@ -0,0 +1,27 @@
+export default function Unforgettable() {
+  return (
+    <section
+      className="flex flex-col h-screen w-full items-center justify-start bg-white my-2"
+      style={{
+        backgroundImage: "url('/coconut-tree.jpg')",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top left",
+        backgroundSize: "cover",
+      }}
+    >
+      <p className="text-lg">Embark on an Unforgettable Adventure</p>
+      <h2 className="text-5xl font-bold text-black my-4">
+        Drive Your Own Tuktuk
+      </h2>
+      <p className="text-lg">
+        Wait, you’re going to let me drive a three wheeler in Sri Lanka? Yes we
+        are and you’re going to LOVE IT!
+      </p>
+      <p className="flex text-center justify-center text-lg my-4 mx-40">
+        We rent tuktuks to travellers who want the freedom to explore Sri Lanka
+        by driving an authentic and reliable rickshaw! Moreover get off the
+        beaten track and find the amazing places other tourists miss.
+      </p>
+    </section>
+  );
+}
