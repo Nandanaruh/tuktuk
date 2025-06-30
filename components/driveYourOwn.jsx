eb90a834ff745b7d335ea57e76a4f284475bc82f
nandanaruh <nandanaruh@gmail.com>
2025-06-30 19:38:48 +0530

    Create drive your own component

---
+++ components/driveYourOwn.jsx
@@ -0,0 +1,29 @@
+export default function DriveYourOwn() {
+  return (
+    <section className="flex flex-col text-center justify-center ">
+      <p
+        className="text-lg font-amatic text-sky-700"
+        style={{
+          fontFamily: "'Amatic SC', cursive",
+          fontSize: "35px",
+          fontWeight: "700",
+        }}
+      >
+        Embark on an Unforgettable Adventure
+      </p>
+      <h2 className="text-5xl font-bold text-black my-4">
+        Drive Your Own Tuktuk
+      </h2>
+      <p className="text-lg text-center">
+        Wait, you’re going to let me drive a three wheeler in Sri Lanka? Yes we
+        are and you’re going to LOVE IT!
+      </p>
+      <p className="text-lg my-4 mx-50">
+        We rent tuktuks to travellers who want the freedom to explore Sri Lanka
+        by driving an authentic and reliable rickshaw! <br />
+        Moreover get off the beaten track and find the amazing places other
+        tourists miss.
+      </p>
+    </section>
+  );
+}
--- components/unforgettable.jsx
+++ components/unforgettable.jsx
@@ -1,4 +1,5 @@
 import FreedomCard from "@/components/freedom-card";
+import DriveYourOwn from "./driveYourOwn";
 
 const cardData = [
   {
@@ -29,29 +30,7 @@ export default function Unforgettable() {
         backgroundSize: "cover",
       }}
     >
-      <p
-        className="text-lg font-amatic text-sky-700"
-        style={{
-          fontFamily: "'Amatic SC', cursive",
-          fontSize: "35px",
-          fontWeight: "700",
-        }}
-      >
-        Embark on an Unforgettable Adventure
-      </p>
-      <h2 className="text-5xl font-bold text-black my-4">
-        Drive Your Own Tuktuk
-      </h2>
-      <p className="text-lg text-center">
-        Wait, you’re going to let me drive a three wheeler in Sri Lanka? Yes we
-        are and you’re going to LOVE IT!
-      </p>
-      <p className="flex text-center justify-center text-lg my-4 mx-50">
-        We rent tuktuks to travellers who want the freedom to explore Sri Lanka
-        by driving an authentic and reliable rickshaw! <br />
-        Moreover get off the beaten track and find the amazing places other
-        tourists miss.
-      </p>
+      <DriveYourOwn />
       <section className="flex flex-row items-center justify-center mx-40">
         <div className="w-full flex flex-col items-center justify-center gap-12 py-12">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
