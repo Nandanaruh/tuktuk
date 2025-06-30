4eedd3c46177f07bb182b8846604402c326aaea5
nandanaruh <nandanaruh@gmail.com>
2025-07-01 01:07:03 +0530

    Create choice your extra component

---
+++ components/choice-your-extras.jsx
@@ -0,0 +1,25 @@
+// Navbar links
+const navLinks = [
+  {
+    icon: "",
+    title: "Surf board rack​",
+    description: "Strap your precious surfboards.",
+  },
+  {
+    icon: "",
+    title: "Seat Belt​",
+    description: "Add an extra buckle of protection.",
+  },
+  {
+    icon: "",
+    title: "Surf board rack​",
+    description: "Strap your precious surfboards.",
+  },
+  {
+    icon: "",
+    title: "Surf board rack​",
+    description: "Strap your precious surfboards.",
+  },
+];
+
+export default navLinks;
