6493bb499806a0f8cd615c6f7e59cce0c607c296
nandanaruh <nandanaruh@gmail.com>
2025-07-02 08:28:18 +0530

    Fix icons on choose your extras

--- components/choice-your-extras.jsx
+++ components/choice-your-extras.jsx
@@ -1,32 +1,32 @@
 // Choice your extras
 const choiceYourExtras = [
   {
-    icon: "/icons/surfboard.svg",
+    icon: "/icons/surf-board.png",
     title: "Surf board rack​",
     description: "Strap your precious surfboards.",
   },
   {
-    icon: "/icons/seatbelt.svg",
+    icon: "/icons/seat-belt.png",
     title: "Seat Belt​",
     description: "Add an extra buckle of protection.",
   },
   {
-    icon: "/icons/baby-seat.svg",
+    icon: "/icons/baby-seat.png",
     title: "Baby Seat​​",
     description: "Comfortable rides for little ones.",
   },
   {
-    icon: "/icons/speaker.svg",
+    icon: "/icons/speaker.png",
     title: "Bluetooth speakers​",
     description: "Create your party on the road.",
   },
   {
-    icon: "/icons/cooler.svg",
+    icon: "/icons/cooler.png",
     title: "Cooler / Esky​",
     description: "Chilled refreshments on-the-go.",
   },
   {
-    icon: "/icons/train.svg",
+    icon: "/icons/train.png",
     title: "Train Transfer​",
     description: "Train travel while we tuk-tuk with your baggage.",
   },
